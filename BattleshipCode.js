788477be8e658f09a8477b3843ef0116bb169e49
Charles Eubanks <146963974+Eubanksch81@users.noreply.github.com>
2024-10-05 16:31:59 -0500

    Saving changes
    Fixing JS code so far

--- BattleshipCode.js
+++ BattleshipCode.js
@@ -1,8 +1,12 @@
 class Piece {
+    #isHit;
+    #xCoord;
+    #yCoord;
+
     constructor() {
-        this.isHit = false;
-        this.xCoord = 0;
-        this.yCoord = 0;
+        this.#isHit = false;
+        this.#xCoord = 0;
+        this.#yCoord = 0;
     }
 
     setXCoord(x) {
@@ -11,7 +15,6 @@ class Piece {
 
     setYCoord(y) {
         this.yCoord = y;
-        "testing testing"
     }
 
     getYCoord() {
@@ -40,50 +43,56 @@ class Piece {
 }
 
 class Ship {
+    #length = 0;
+    #shipPieces;
+
     constructor(length) {
-        this.length = length;
-        this.shipPieces = [];
+        this.#length = length;
+        this.#shipPieces = [];
         this.addPieces();
     }
 
     addPieces() {
-        for (let i = 0; i < this.length; i++) {
-            const piece = new Piece();
+        for (let i = 0; i < this.#length; i++) {
+            let piece = new Piece();
             piece.setPieceNum(i + 1);
-            this.shipPieces.push(piece);
+            this.#shipPieces.push(piece);
         }
     }
 
     getLength() {
-        return this.length;
+        return this.#length;
     }
 
     getPieceXCoord(pieceNum) {
-        return this.shipPieces[pieceNum].getXCoord();
+        return this.#shipPieces[pieceNum].getXCoord();
     }
 
     getPieceYCoord(pieceNum) {
-        return this.shipPieces[pieceNum].getYCoord();
+        return this.#shipPieces[pieceNum].getYCoord();
     }
 
     hitPiece(pieceNum) {
-        this.shipPieces[pieceNum].setHit(true);
+        this.#shipPieces[pieceNum].setHit(true);
     }
 
     isPieceHit(pieceNum) {
-        return this.shipPieces[pieceNum].isHit();
+        return this.#shipPieces[pieceNum].isHit();
     }
 
     setPieceLocation(pieceNum, xCoord, yCoord) {
-        this.shipPieces[pieceNum].setXCoord(xCoord);
-        this.shipPieces[pieceNum].setYCoord(yCoord);
+        this.#shipPieces[pieceNum].setXCoord(xCoord);
+        this.#shipPieces[pieceNum].setYCoord(yCoord);
     }
 }
 
 class BattleMap {
+    #gameBoard;
+    #ships;
+
     constructor() {
-        this.gameBoard = Array.from({ length: 10 }, () => Array(10).fill(0));
-        this.ships = [
+        this.#gameBoard = Array.from({ length: 10 }, () => Array(10).fill(0));
+        this.#ships = [
             new Ship(5),
             new Ship(4),
             new Ship(3),
@@ -93,14 +102,14 @@ class BattleMap {
     }
 
     setFirstPieceOnBoard(shipNum, xCoord, yCoord) {
-        if (this.isValidCoords(xCoord, yCoord) && this.gameBoard[xCoord][yCoord] !== 1) {
-            this.ships[shipNum].setPieceLocation(0, xCoord, yCoord);
-            this.gameBoard[xCoord][yCoord] = 1;
+        if (this.isValidCoords(xCoord, yCoord) && this.#gameBoard[xCoord][yCoord] !== 1) {
+            this.#ships[shipNum].setPieceLocation(0, xCoord, yCoord);
+            this.#gameBoard[xCoord][yCoord] = 1;
 
-            if (this.createPotentialPieces(this.ships[shipNum].getLength(), xCoord, yCoord)) {
+            if (this.createPotentialPieces(this.#ships[shipNum].getLength(), xCoord, yCoord)) {
                 return true;
             } else {
-                this.gameBoard[xCoord][yCoord] = 0;
+                this.#gameBoard[xCoord][yCoord] = 0;
                 return false;
             }
         }
@@ -108,22 +117,22 @@ class BattleMap {
     }
 
     setOtherPiecesOnBoard(shipNum, xCoord, yCoord) {
-        if (this.gameBoard[xCoord][yCoord] === 3) {
-            const initialX = this.ships[shipNum].getPieceXCoord(0);
-            const initialY = this.ships[shipNum].getPieceYCoord(0);
-            this.removePotentialPieces(this.ships[shipNum].getLength(), initialX, initialY);
-            this.gameBoard[xCoord][yCoord] = 1;
-            this.ships[shipNum].setPieceLocation(this.ships[shipNum].getLength() - 1, xCoord, yCoord);
+        if (this.#gameBoard[xCoord][yCoord] === 3) {
+            const initialX = this.#ships[shipNum].getPieceXCoord(0);
+            const initialY = this.#ships[shipNum].getPieceYCoord(0);
+            this.removePotentialPieces(this.#ships[shipNum].getLength(), initialX, initialY);
+            this.#gameBoard[xCoord][yCoord] = 1;
+            this.#ships[shipNum].setPieceLocation(this.#ships[shipNum].getLength() - 1, xCoord, yCoord);
 
             if (initialX === xCoord) {
-                for (let i = 0; i < this.ships[shipNum].getLength(); i++) {
-                    this.gameBoard[xCoord][yCoord + (initialY < yCoord ? -i : i)] = 1;
-                    this.ships[shipNum].setPieceLocation(i, xCoord, yCoord + (initialY < yCoord ? -i : i));
+                for (let i = 0; i < this.#ships[shipNum].getLength(); i++) {
+                    this.#gameBoard[xCoord][yCoord + (initialY < yCoord ? -i : i)] = 1;
+                    this.#ships[shipNum].setPieceLocation(i, xCoord, yCoord + (initialY < yCoord ? -i : i));
                 }
             } else {
-                for (let i = 0; i < this.ships[shipNum].getLength(); i++) {
-                    this.gameBoard[xCoord + (initialX < xCoord ? -i : i)][yCoord] = 1;
-                    this.ships[shipNum].setPieceLocation(i, xCoord + (initialX < xCoord ? -i : i), yCoord);
+                for (let i = 0; i < this.#ships[shipNum].getLength(); i++) {
+                    this.#gameBoard[xCoord + (initialX < xCoord ? -i : i)][yCoord] = 1;
+                    this.#ships[shipNum].setPieceLocation(i, xCoord + (initialX < xCoord ? -i : i), yCoord);
                 }
             }
             return true;
@@ -132,16 +141,16 @@ class BattleMap {
     }
 
     updateHitOrMiss(xCoord, yCoord) {
-        if (this.gameBoard[xCoord][yCoord] === 0) {
-            this.gameBoard[xCoord][yCoord] = 4; // Miss
-        } else if (this.gameBoard[xCoord][yCoord] === 1) {
-            this.gameBoard[xCoord][yCoord] = 2; // Hit
+        if (this.#gameBoard[xCoord][yCoord] === 0) {
+            this.#gameBoard[xCoord][yCoord] = 4; // Miss
+        } else if (this.#gameBoard[xCoord][yCoord] === 1) {
+            this.#gameBoard[xCoord][yCoord] = 2; // Hit
             this.hitPiece(xCoord, yCoord);
         }
     }
 
     hitPiece(xCoord, yCoord) {
-        for (const ship of this.ships) {
+        for (const ship of this.#ships) {
             for (let j = 0; j < ship.getLength(); j++) {
                 if (ship.getPieceXCoord(j) === xCoord && ship.getPieceYCoord(j) === yCoord) {
                     ship.hitPiece(j);
@@ -151,21 +160,21 @@ class BattleMap {
     }
 
     checkWin() {
-        return this.ships.some(ship => ship.getLength().some((_, j) => !ship.isPieceHit(j)));
+        return this.#ships.some(ship => ship.getLength().some((_, j) => !ship.isPieceHit(j)));
     }
 
     removePotentialPieces(length, xCoord, yCoord) {
-        if (xCoord + (length - 1) < 10 && this.gameBoard[xCoord + (length - 1)][yCoord] === 3) {
-            this.gameBoard[xCoord + (length - 1)][yCoord] = 0;
+        if (xCoord + (length - 1) < 10 && this.#gameBoard[xCoord + (length - 1)][yCoord] === 3) {
+            this.#gameBoard[xCoord + (length - 1)][yCoord] = 0;
         }
-        if (xCoord - (length - 1) > -1 && this.gameBoard[xCoord - (length - 1)][yCoord] === 3) {
-            this.gameBoard[xCoord - (length - 1)][yCoord] = 0;
+        if (xCoord - (length - 1) > -1 && this.#gameBoard[xCoord - (length - 1)][yCoord] === 3) {
+            this.#gameBoard[xCoord - (length - 1)][yCoord] = 0;
         }
-        if (yCoord + (length - 1) < 10 && this.gameBoard[xCoord][yCoord + (length - 1)] === 3) {
-            this.gameBoard[xCoord][yCoord + (length - 1)] = 0;
+        if (yCoord + (length - 1) < 10 && this.#gameBoard[xCoord][yCoord + (length - 1)] === 3) {
+            this.#gameBoard[xCoord][yCoord + (length - 1)] = 0;
         }
-        if (yCoord - (length - 1) > -1 && this.gameBoard[xCoord][yCoord - (length - 1)] === 3) {
-            this.gameBoard[xCoord][yCoord - (length - 1)] = 0;
+        if (yCoord - (length - 1) > -1 && this.#gameBoard[xCoord][yCoord - (length - 1)] === 3) {
+            this.#gameBoard[xCoord][yCoord - (length - 1)] = 0;
         }
     }
 
@@ -184,13 +193,13 @@ class BattleMap {
             for (let i = 1; i < length; i++) {
                 const newX = xCoord + dx * i;
                 const newY = yCoord + dy * i;
-                if (this.gameBoard[newX]?.[newY] === 1) {
+                if (this.#gameBoard[newX]?.[newY] === 1) {
                     valid = false;
                     break;
                 }
             }
             if (valid && this.isValidCoords(xCoord + dx * (length - 1), yCoord + dy * (length - 1))) {
-                this.gameBoard[xCoord + dx * (length - 1)][yCoord + dy * (length - 1)] = 3;
+                this.#gameBoard[xCoord + dx * (length - 1)][yCoord + dy * (length - 1)] = 3;
                 canPlace = true;
             }
         }
@@ -204,7 +213,7 @@ class BattleMap {
     toString() {
         let toReturn = '   A B C D E F G H I J\n';
         for (let i = 0; i < 10; i++) {
-            toReturn += (i) + '  ' + this.gameBoard[i].join(' ') + '\n';
+            toReturn += (i) + '  ' + this.#gameBoard[i].join(' ') + '\n';
         }
         return toReturn;
     }
@@ -214,7 +223,7 @@ class BattleMap {
         for (let i = 0; i < 10; i++) {
             toReturn += i + ' ';
             for (let j = 0; j < 10; j++) {
-                switch (this.gameBoard[j][i]) {
+                switch (this.#gameBoard[j][i]) {
                     case 0:
                     case 1:
                         toReturn += '□ ';
@@ -272,6 +281,8 @@ class Game {
     placeShips(map) {
         // Placeholder for ship placement logic
         console.log("Place your ships on the board");
+        this.printOpenMap(map);
+        console.log("Please place the first piece of your first ship.");
         // Implement your ship placement logic here
     }
 
