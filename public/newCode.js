389edd4b88740602979960a459e3261e415b42c1
Charles Eubanks <146963974+Eubanksch81@users.noreply.github.com>
2024-10-28 11:40:27 -0500

    Forgot to change mainCoord1
    whoops

--- public/newCode.js
+++ public/newCode.js
@@ -6,7 +6,8 @@ window.onload = function() {
     for (let i = 0; i < 10; i++) {
         for (let j = 0; j < 10; ++j) {
             let iValue = i + ASCIIbase;
-            oStrCoords += `<button type="button" class="boxElement id="${String.fromCharCode(iValue)}${j}"}> ${String.fromCharCode(iValue)} </button>`;
+            let idName = String.fromCharCode(iValue) + j.toString();
+            oStrCoords += `<button type="button" class="boxElement id="${idName}"}> ${String.fromCharCode(iValue)} </button>`;
         }
     }
     htmlCoords.innerHTML = oStrCoords;
@@ -17,14 +18,9 @@ window.onload = function() {
         for (let j = 1; j < 11; ++j) {
             let iValue = i + ASCIIbase;
             let idName = String.fromCharCode(iValue) + j.toString();
-            console.log(idName);
+            // console.log(idName);
             oStrCoords += `<button type="button" class="boxElement id="${idName}"}> ${String.fromCharCode(iValue)} </button>`;
         }
     }
     htmlCoords.innerHTML = oStrCoords;
-
-
-    for (let i = 1; i < 201; ++i) {
-
-    }
 };
