bd6c25db32cde65601582fb3dd54c0c74ac39b9b
PatGally <149409313+PatGally@users.noreply.github.com>
2024-12-13 20:38:46 -0800

    Validation for create user page

--- public/createUser.js
+++ public/createUser.js
@@ -5,15 +5,34 @@ document.getElementById('createForm').addEventListener('submit', async function(
     const password = document.getElementById('password').value;
     const redoPassword = document.getElementById('reEnterPassword').value;
     const errorMessage = document.getElementById('error-message');
-
     errorMessage.textContent = '';
+
     //Validation
 
     if(username === '' || password === '' || redoPassword === ''){
         errorMessage.textContent = 'All fields are required.';
         return;
     }
-    //TODO: Validation checks
+    if(password.length < 6){
+        errorMessage.textContent = 'Password must be at least 6 characters long';
+        return;
+    }
+    let i = 0;
+    let found = false;
+    while( !found && i < password.length){
+        if(password[i] === password[i].toUpperCase() && isNaN(password[i]) ){
+            found = true;
+        }
+        i+=1;
+    }
+    if(!found){
+        errorMessage.textContent = 'At least 1 character must be upper case';
+        return;
+    }
+    if(password !== redoPassword){
+        errorMessage.textContent = 'Passwords must match';
+        return;
+    }
 
     try{
         const response = await axios.post('http://localhost:3000/BattleShip/public/createLogin', { username, password });
